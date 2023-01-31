6d9b46d304a6568fb458bab5e848cb129b548d80
gabe-husain <gabe.husain@gmail.com>
2023-01-30 17:27:13 -0800

    fuck dotenv

--- flask_dir/flask_app.py
+++ flask_dir/flask_app.py
@@ -1,8 +1,9 @@
 from flask import Flask, Blueprint, session
 from routesFolder.routes import routes_file
 import os
+from dotenv import load_dotenv, find_dotenv
 
-
+load_dotenv(find_dotenv())
 app = Flask(__name__)
 app.config.from_object("configFile.DevelopmentConfig")
 app.config["SESSION_COOKIE_NAME"] = 'Spotify Sesh'
