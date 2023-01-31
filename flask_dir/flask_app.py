dcdaab330380782fb8879eae637fe54938224b29
gabe-husain <gabe.husain@gmail.com>
2023-01-30 21:52:57 -0800

    .env fix?

--- flask_dir/flask_app.py
+++ flask_dir/flask_app.py
@@ -1,9 +1,11 @@
 from flask import Flask, Blueprint, session
 from routesFolder.routes import routes_file
 import os
+
 from dotenv import load_dotenv, find_dotenv
 
-load_dotenv(find_dotenv())
+load_dotenv(".env")
+
 app = Flask(__name__)
 app.config.from_object("configFile.DevelopmentConfig")
 app.config["SESSION_COOKIE_NAME"] = 'Spotify Sesh'
